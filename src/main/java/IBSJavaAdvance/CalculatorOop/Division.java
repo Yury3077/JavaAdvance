6a083d38476ec0fbf5560d07bbe8bd117cf1b876
Yury Balykov <juser89@mail.ru>
2025-06-20 16:09:53 +0300

    Task-5 exceptions

--- src/main/java/IBSJavaAdvance/CalculatorOop/Division.java
+++ src/main/java/IBSJavaAdvance/CalculatorOop/Division.java
@@ -3,11 +3,15 @@ package IBSJavaAdvance.CalculatorOop;
 public class Division implements Operations {
     @Override
     public Double doOperation(Double number1, Double number2) {
-        if (number2.intValue() == 0) {
+        try {
+            int res = number1.intValue() / number2.intValue();
+            return number1 / number2;
+        }
+        catch (ArithmeticException e) {
+            e.getStackTrace();
             System.out.println("Деление на 0!");
             return null;
-        } else {
-            return number1 / number2;
+
         }
     }
 }
